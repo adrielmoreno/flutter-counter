f7239faf8e3c9635410299b7ec6328b20593bdf0
MorenoCreativo <adrielmoreno@hotmail.com>
2022-07-05 14:43:49 +0200

    Contador finalizado

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,9 +4,11 @@ import 'package:flutter/material.dart';
 // import 'package:contador/src/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Center(
         child: ContadorPage(),
--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ContadorPage extends StatefulWidget {
+  const ContadorPage({Key? key}) : super(key: key);
+
   @override
   createState() => _ContadorPageState();
 }
@@ -12,26 +14,53 @@ class _ContadorPageState extends State<ContadorPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Contador'), centerTitle: true),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Clicks:', style: _textStyle),
-            Text('$_conteo', style: _textStyle),
-          ],
+        appBar: AppBar(title: const Text('Contador'), centerTitle: true),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Clicks:', style: _textStyle),
+              Text('$_conteo', style: _textStyle),
+            ],
+          ),
         ),
-      ),
+        floatingActionButton: _crearBotones()
+        // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        );
+  }
 
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.plus_one),
-        onPressed: () {
-          setState(() {
-            _conteo++;
-          });
-        },
-      ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        const SizedBox(),
+        FloatingActionButton(
+          onPressed: _restar,
+          child: const Icon(Icons.remove),
+        ),
+        const SizedBox(),
+        FloatingActionButton(
+          onPressed: _reset,
+          child: const Icon(Icons.exposure_zero_sharp),
+        ),
+        const SizedBox(),
+        FloatingActionButton(
+          onPressed: _sumar,
+          child: const Icon(Icons.add),
+        )
+      ],
     );
+  }
+
+  void _sumar() {
+    setState(() => _conteo++);
+  }
+
+  void _restar() {
+    setState(() => {if (_conteo > 0) _conteo--});
+  }
+
+  void _reset() {
+    setState(() => _conteo = 0);
   }
 }
